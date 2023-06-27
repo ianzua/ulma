80e22fcb26cf0d14141e39447d209f1c43f87a89
ianzua <iantia@azk.eus>
2023-06-27 09:40:30 +0200

    last changes

--- js/login.js
+++ js/login.js
@@ -1,34 +1,242 @@
 var objPeople = [
     {
-        username : "ulma",
+        username : "ULMA",
         password : "ulmarenewal"
     },
     {
-        username : "bralo",
+        username : "LOGISIETE",
         password : "ulmarenewal"
     },
     {
-        username : "catelsa",
+        username : "CONSUM",
         password : "ulmarenewal"
     },
     {
-        username : "tello",
+        username : "HOFMANN",
         password : "ulmarenewal"
     },
     {
-        username : "finsa",
+        username : "GRESPANIA",
         password : "ulmarenewal"
     },
     {
-        username : "glaxo",
+        username : "SALVESEN",
         password : "ulmarenewal"
     },
     {
-        username : "vemare",
+        username : "FEGEMU",
         password : "ulmarenewal"
     },
     {
-        username : "felixsolis",
+        username : "ROGE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "BRALO",
+        password : "ulmarenewal"
+    },
+    {
+        username : "CONDALS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "SIMON",
+        password : "ulmarenewal"
+    },
+    {
+        username : "QF+",
+        password : "ulmarenewal"
+    },
+    {
+        username : "INDUSTRIALZAPATERA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "DANONE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "TRACE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "CAF",
+        password : "ulmarenewal"
+    },
+    {
+        username : "COFERDROZA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "CATELSA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "URGO",
+        password : "ulmarenewal"
+    },
+    {
+        username : "EROSKI",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ORONA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "UBIS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "JASI",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ULMAFORJA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ABC",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ARTADI",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ABACUS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "SNCF",
+        password : "ulmarenewal"
+    },
+    {
+        username : "PERNODRICARD",
+        password : "ulmarenewal"
+    },
+    {
+        username : "WALTERMARTINEZ",
+        password : "ulmarenewal"
+    },
+    {
+        username : "SOLER&PALAU",
+        password : "ulmarenewal"
+    },
+    {
+        username : "TELLO",
+        password : "ulmarenewal"
+    },
+    {
+        username : "KRAFFT",
+        password : "ulmarenewal"
+    },
+    {
+        username : "SAMSE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "PROCLINIC",
+        password : "ulmarenewal"
+    },
+    {
+        username : "LACOR",
+        password : "ulmarenewal"
+    },
+    {
+        username : "BELENUS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "CEYS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "VALENTINE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "FINSA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "GLAXO",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ACMARCA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "CANDELSA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "VEMARE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "AUXILIARCONSERVERA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "KRAFFT",
+        password : "ulmarenewal"
+    },
+    {
+        username : "FELIXSOLIS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "GKN",
+        password : "ulmarenewal"
+    },
+    {
+        username : "EHLIS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "SPB",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ZARAHOME",
+        password : "ulmarenewal"
+    },
+    {
+        username : "FORUM",
+        password : "ulmarenewal"
+    },
+    {
+        username : "LUG",
+        password : "ulmarenewal"
+    },
+    {
+        username : "SYSTEMU",
+        password : "ulmarenewal"
+    },
+    {
+        username : "JMA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "CUETARA",
+        password : "ulmarenewal"
+    },
+    {
+        username : "BONDUELLE",
+        password : "ulmarenewal"
+    },
+    {
+        username : "TRW",
+        password : "ulmarenewal"
+    },
+    {
+        username : "TELIFRAIS",
+        password : "ulmarenewal"
+    },
+    {
+        username : "ELAY",
         password : "ulmarenewal"
     },
 ]
